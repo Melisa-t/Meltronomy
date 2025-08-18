71e97642ce913e22d458a6a0878756c7e7b68189
Melisa Lachev <melisatormac55@gmail.com>
2025-08-18 19:40:33 +0300

    fixed styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,14 +7,20 @@ export default function Main() {
   );
 }
 
+function handleFormSubmit(FormData) {
+  const ingredient = FormData.get("ingredient");
+  if (!ingredient) return;
+  console.log(ingredient);
+}
+
 function InputBox() {
   return (
     <section className="input-section">
-      <div className="input-box">
+      <form action={handleFormSubmit} className="input-box">
         <label>Ingredients at home</label>
-        <input type="text" placeholder="e.g. oregano" />
-      </div>
-      <button>Add Ingredients</button>
+        <input name="ingredient" type="text" placeholder="e.g. oregano" />
+        <button>Add Ingredients</button>
+      </form>
     </section>
   );
 }
